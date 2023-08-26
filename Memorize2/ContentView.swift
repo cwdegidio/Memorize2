fb511587e5982d1f11ecf002235d147418053314
Curtis W. DeGidio <curtis.degidio@gmail.com>
2023-08-26 16:28:20 -0500

    Initial Commit - created CardView - Added isFaceUp toggle - Added vehicle emoji array - Organized cards into grid

--- Memorize2/ContentView.swift
+++ Memorize2/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis: Array<String> = []
+    let vehicles: Array<String> = [
+        "✈️", "✈️", "🛻", "🛻", "🚜", "🚜", "🚗", "🚗",
+        "🚛", "🚛", "🚔", "🚔", "⛵️", "⛵️", "🚎", "🚎"
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+                ForEach(0..<vehicles.count, id: \.self) { index in
+                    CardView(content: vehicles[index])
+                        .aspectRatio(2/3, contentMode: .fit)
+                }
+            }
         }
+        .foregroundColor(.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = true
+    
+    var body: some View {
+        let base = RoundedRectangle(cornerRadius: 12)
+        
+        ZStack {
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 5)
+                Text(content).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 

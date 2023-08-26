6f9243200bf2d582d15b7fca313f01fd2c4fad85
Curtis W. DeGidio <curtis.degidio@gmail.com>
2023-08-26 15:12:41 -0500

    Initial Commit

---
+++ Memorize2/Memorize2App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2App.swift
+//  Memorize2
+//
+//  Created by Curtis DeGidio on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
